eccf01c26218922384e415463508299b819ecfbf
Isaac Ayomide <okunlolaa3@outlook.com>
2024-03-16 21:12:13 +0100

    Added a success message for correct email

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const errorMsg = document.querySelector(".error__message");
 const submitMail = function () {
   if (inputEl.value.includes(".") && inputEl.value.includes("@")) {
     console.log("Success");
-    errorMsg.style.display = "none";
-    inputEl.style.border = `1px solid hsl(223, 100%, 88%)`;
+    
+    errorMsg.style.display = "block";
+    errorMsg.textContent = 'Thank you for subscribing!'
+    errorMsg.style.color = 'Green'
+    inputEl.style.border = `1px solid green`;
     inputEl.value = "";
   } else if (inputEl.value === "") {
     errorMsg.style.display = "block";
     errorMsg.innerHTML = "Whoops! It looks like you forgot to add your email";
   } else {
-    errorMsg.innerHTML = 'Please provide a valid email address'
+    errorMsg.innerHTML = "Please provide a valid email address";
     errorMsg.style.display = "block";
     inputEl.style.border = `1px solid red`;
   }
